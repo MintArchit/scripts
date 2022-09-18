289f572c827c964aa6c986486b54617477c8c581
mintarchit <mintarchit@gmail.com>
2022-09-18 17:58:09 +0000

    added protection against accidentally turning off screen

--- backlight.sh
+++ backlight.sh
@@ -10,11 +10,27 @@ pth="/sys/class/backlight/*"
 max=$(cat $pth/max_brightness)
 current=$(cat $pth/actual_brightness)
 
-#sudo -v
+sudo -v
 num="whiptail --inputbox \"max brightness: $max \\ncurrent brightness: $current\" 15 60 3>&1 1>&2 2>&3"
 selected=$(eval $num)
 
-echo $selected | sudo tee /sys/class/backlight/*/brightness
+cmd="echo $selected | sudo tee /sys/class/backlight/*/brightness"
+
+if [ $? -eq 0 ] && echo "whiptail was successful"
+then
+	if [[ "$selected" == "0" ]]
+	then
+		echo "0 not allowed, since that turns off the screen"
+	else
+		eval "$cmd"
+	fi
+elif [ $? -eq 1 ] 
+then
+	# echo "whiptail was canceled"
+	exit 0
+else
+	exit 1
+fi
 
 #num=$(whiptail --inputbox "max brightness: $max \ncurrent brightness: $current" 15 60)
 
