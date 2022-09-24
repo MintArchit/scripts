d5df9b74c4197674a873bdd5956067e6ac1b41a6
mintarchit <mintarchit@gmail.com>
2022-09-24 16:14:45 +0000

    update

--- shconfig.sh
+++ shconfig.sh
@@ -3,28 +3,16 @@
 # author:	mintarchit
 # version:	1.0
 
-cmds=(
-	$tst
-	#$ohmyzsh
-	#$powerlevel10
-	#$sed_zsh
-)
-
-tst="echo tesst"
 
 ohmyzsh="sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)""
 
 powerlevel10="git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
-sed_zsh="sed -e s@ZSH_THEME=\"robbyrussell\"@ZSH_THEME=\"powerlevel10k/powerlevel10k\"@g  -i dev_zshrc"
 
-#echo "cmd : ${cmds[@]}"
+#$ohmyzsh
+#$powerlevel10
+cp ../dotfiles/sh/.zshrc ~
 
-$ohmyzsh
-$powerlevel10
-
+#chsh -s /bin/zsh
 #cat /etc/shells
-
-chsh -s /bin/zsh
-
 echo "logout && login, so the change can take effect"
