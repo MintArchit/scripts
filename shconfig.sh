23b39313b70467913fa3b7382476371bf56c6f8e
mintarchit <mintarchit@gmail.com>
2022-09-24 15:47:57 +0000

    init

--- shconfig.sh
+++ shconfig.sh
@@ -10,7 +10,7 @@ cmds=(
 	#$sed_zsh
 )
 
-tst="echo tst"
+tst="echo tesst"
 
 ohmyzsh="sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)""
 
@@ -18,4 +18,13 @@ powerlevel10="git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $
 
 sed_zsh="sed -e s@ZSH_THEME=\"robbyrussell\"@ZSH_THEME=\"powerlevel10k/powerlevel10k\"@g  -i dev_zshrc"
 
-eval "${cmds[@]}"
+#echo "cmd : ${cmds[@]}"
+
+$ohmyzsh
+$powerlevel10
+
+#cat /etc/shells
+
+chsh -s /bin/zsh
+
+echo "logout && login, so the change can take effect"
