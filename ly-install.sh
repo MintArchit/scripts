1727619c20d5daace337b7c6adf757f511fcc902
mintarchit <mintarchit@gmail.com>
2022-09-26 10:17:35 +0000

    uncomment

--- ly-install.sh
+++ ly-install.sh
@@ -8,8 +8,8 @@ current_dir=$(pwd)
 
 mkdir -p ~/bin/ && cd ~/bin/
 file='ly_0.5.0'
-#wget 'https://github.com/fairyglade/ly/releases/download/v0.5.0/ly_0.5.0.zip'
-#unzip "$file.zip"
+wget 'https://github.com/fairyglade/ly/releases/download/v0.5.0/ly_0.5.0.zip'
+unzip "$file.zip"
 sudo $file/install.sh
 sudo systemctl enable ly.service
 sudo systemctl start ly.service
