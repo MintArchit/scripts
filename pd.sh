0b858e905edd58b9a6fbd1b89855263e36c7d155
mintarchit <mintarchit@gmail.com>
2022-09-27 23:15:20 +0100

    init

---
+++ pd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -li
+
+# author:	mintarchit
+# version:	1.0
+
+#echo "$BASH_SOURCE"
+#shopt -s expand_aliases
+#source $BASH_SOURCE
+#alias pd=proot-distro
+#alias ubuntu='proot-distro login ubuntu --user admin --isolated'
+proot-distro login ubuntu --user admin --isolated
