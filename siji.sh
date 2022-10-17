c37cd16da9d475d77f7149dcbbb72a38043f3ae0
mintarchit <mintarchit@gmail.com>
2022-10-17 17:01:15 +0200

    new

---
+++ siji.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# author:	mintarchit
+# version:	1.0
+
+gh repo clone https://github.com/stark/siji $HOME/siji
+$HOME/siji/install.sh
---
+++ utils/headset.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# author:	mintarchit
+# version:	1.0
+
+bluetoothctl connect 2C:4D:79:B7:06:1D
