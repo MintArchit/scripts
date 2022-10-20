58a0b6222e6d1aa9fe361ddec51bd0c0037cc7d1
mintarchit <mintarchit@gmail.com>
2022-10-20 16:23:39 +0200

    update

--- config/uni-fs.sh
+++ config/uni-fs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # author:	mintarchit
-# version:	1.3
+# version:	2.1
 
 oldcmd="mkdir -p ./*/{lec,sh}"
-for dir in */; do mkdir -- "$dir/{lec,sh}"; done
+for dir in */; do mkdir -- "$dir"/{lec,sh}; done
 #echo "$cmd"
 #eval "$cmd"
