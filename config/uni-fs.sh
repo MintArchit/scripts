1cf3a6830e7a6c53105ff30f11c9c627be1795b6
mintarchit <mintarchit@gmail.com>
2022-10-20 12:09:37 +0200

    scripts works

--- config/uni-fs.sh
+++ config/uni-fs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # author:	mintarchit
-# version:	1.2
+# version:	1.3
 
 oldcmd="mkdir -p ./*/{lec,sh}"
-cmd="for dir in */; do mkdir -- "$dir/{lec,sh}"; done"
-echo "$cmd"
-eval "$cmd"
+for dir in */; do mkdir -- "$dir/{lec,sh}"; done
+#echo "$cmd"
+#eval "$cmd"
