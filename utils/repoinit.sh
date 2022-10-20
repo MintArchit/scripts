33a4159fe3f7d7a216ff62b3e26c41c7563c4e86
mintarchit <mintarchit@gmail.com>
2022-10-20 11:29:33 +0200

    update

--- utils/repoinit.sh
+++ utils/repoinit.sh
@@ -5,12 +5,22 @@
 
 # repoinit.sh [[repo-name]]
 
+privacy='public'
+while true; do
+	read -p "should this repo be public (y/n) " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) privacy='private' break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 echo "# $1" >> README.md
 git init
 git add README.md
 git commit -m "repo init"
 git branch -M main
-gh repo create $1 --public --source=. --remote=upstream
+gh repo create $1 --$privacy --source=. --remote=upstream
 git remote add origin https://github.com/MintArchit/$1.git
 git push -u origin main
 
