58525526a72ec7d643e1973a96689ba5415e49fd
mintarchit <mintarchit@gmail.com>
2022-11-01 23:47:23 +0100

    init dependecies install/polybar.sh

---
+++ install/polybar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# author:	mintarchit
+# version:	1.0
+
+#apt install build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
+
+apt install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
