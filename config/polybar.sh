982eda6610e988f86bd7451b860cf86054c3344b
mintarchit <mintarchit@gmail.com>
2022-11-02 17:43:07 +0100

    split file

--- config/polybar.sh
+++ config/polybar.sh
@@ -6,3 +6,4 @@
 mkdir $HOME/.config/polybar
 cp $HOME/ghub/dotfiles/polybar/launch.sh $HOME/.config/polybar/.
 cp $HOME/ghub/dotfiles/polybar/config.ini $HOME/.config/polybar/.
+cp $HOME/ghub/dotfiles/polybar/colors.ini $HOME/.config/polybar/.
