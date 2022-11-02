da07f180165266961195759b56a70c8efd958b10
mintarchit <mintarchit@gmail.com>
2022-11-02 14:26:57 +0100

    update

---
+++ install/rofi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# author:	mintarchit
+# version:	1.0
+
+apt install autoconf automake pkg-config flex check bison xcb libxkbcommon-x11-dev libxcb-xinerama0-dev libstartup-notification0-dev
