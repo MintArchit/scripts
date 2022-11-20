2880d37eaf1115be8e8526d532a9da9417c3a2f6
mintarchit <mintarchit@gmail.com>
2022-11-20 16:23:01 +0100

    enhance rofi script

--- config/rofi.sh
+++ config/rofi.sh
@@ -3,5 +3,8 @@
 # author:	mintarchit
 # version:	1.0
 
-cp $HOME/ghub/dotfiles/rofi/config.rasi $HOME/.config/rofi/
-cp $HOME/ghub/dotfiles/rofi/onedark.rasi $HOME/.config/rofi/
+pth="$HOME/.config/rofi"
+dot="$HOME/ghub/dotfiles/rofi/."
+
+[ ! -d "$pth" ] && mkdir -p "$pth"
+find $dot -maxdepth 1 ! -name ".*" | xargs -i cp {} -r $pth
